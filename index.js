e74d0cce4d59eaa4de3e22e44b6103f179feb0cf
Paul Collins <paul.collins@severalpens.com>
2025-02-05 15:53:19 +1100

    update port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 8080;
 
 const { Client } = require('@elastic/elasticsearch');
 const client = new Client({
