f101e065b1f9c6ce16f6d4419acfbdfe9e34cd10
Paul Collins <paul.collins@severalpens.com>
2025-02-11 15:49:52 +1100

    update

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api/index');
 var faqsRouter = require('./routes/api/faqs');
-var embedRouter = require('./routes/api/embed');
 var createIndex = require('./routes/api/createIndex');
 var app = express();
 const corsOptions = {
@@ -38,7 +37,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 app.use('/api/faqs', faqsRouter);
-app.use('/api/embed', embedRouter);
 app.use('/api/createIndex', createIndex);
 
 app.get('/test', async (req, res) => {
